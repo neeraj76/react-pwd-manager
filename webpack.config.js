5739476ad431e359707965ac41f2a7bd58b8581c
Neeraj Gupta <neeraj@finnirv.com>
2019-05-02 22:14:24 +0530

    Initial commit for a basic webpack npm app

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const PATHS = {
+    app: path.resolve(__dirname,'app'),
+    build: path.resolve(__dirname,'build')
+};
+module.exports = {
+    entry: {
+        app: PATHS.app + "/index.js"
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
+};
